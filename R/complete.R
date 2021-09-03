5c1a1a793a82ebf0123963561653baaac16a892e
Michael Culshaw-Maurer <culshawmaurer@email.arizona.edu>
2021-09-03 11:03:01 -0500

    Adding complete data

---
+++ R/complete.R
@@ -0,0 +1,23 @@
+#' Complete survey data.
+#'
+#' Data on individual rodents captured at the Portal Project and the plots they were captured in.
+#'
+#' @source Portal Project Teaching Database,
+#'  \url{https://doi.org/10.6084/m9.figshare.1314459}
+#' @format A data frame with columns:
+#' \describe{
+#' \item{record_id}{Unique record identifier}
+#' \item{month}{Month captured}
+#' \item{day}{Day captured}
+#' \item{year}{Year captured}
+#' \item{plot_id}{Plot individual was captured on}
+#' \item{species_id}{Species identifier}
+#' \item{sex}{Sex of the individual}
+#' \item{hindfoot_length}{Length of a back foot in mm}
+#' \item{weight}{Weight of the individual in g' Species data.}
+#' \item{genus}{The genus of the species}
+#' \item{species}{The latin species name}
+#' \item{taxa}{General taxonomic category of the species}
+#' \item{plot_type}{Type of experimental manipulation conducted on the plot}
+#' }
+"complete"
